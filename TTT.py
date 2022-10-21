ab382e387d4842d01c25a5b72fafbab3c1fb0150
andersona13 <andersona13@wit.edu>
2022-10-21 10:34:07 -0400

    Added generic functions to implement in TicTacToe

---
+++ TTT.py
@@ -0,0 +1,17 @@
+grid = [[2,2,2]
+        [2,2,2]
+        [2,2,2]]
+def checkWinVert():
+    pass
+def checkWinHoriz():
+    pass
+def checkWinDiag():
+    pass
+def printGrid():
+    pass
+
+
+gameOver = False
+
+while(not(gameOver)):
+    
