3f87b50aeda9e2a047c84a8ba2b19eef7a9d9fca
andersona13 <andersona13@wit.edu>
2022-10-21 21:43:21 -0400

    Uploading what I have in the current moment

--- TTT.py
+++ TTT.py
@@ -1,3 +1,15 @@
+'''
+Key:
+    0 = o
+    1 = x
+    2 = _
+    10 = O
+    11 = X
+'''
+
+from audioop import add
+
+
 grid = [[2, 2, 2], 
         [2, 2, 2], 
         [2, 2, 2]]
@@ -18,8 +30,28 @@ def printGrid():
                 print('_', end=' ')
         print('')
 
-gameOver = False
+def addSymbol(symbol, location):
+    """
+    Adds symbol to grid at location\n
+    Args:
+        symbol (int): an integer that (if less than 2) will be added to the grid
+        location (int[]): acts as coordinates in the grid and specifies where to put the symbol
+    """
+    if(symbol < 2):
+        grid[location[0]][location[1]] = symbol
+    
+    
 
+gameOver = False
+printGrid()
+currentPlayer = 1
+message = f'Player {currentPlayer}, please enter a position'
 while(not(gameOver)):
+    message = f'Player {currentPlayer}, please enter a position'
+    print(message)
+    currentPlayer+=1
+    
+    addSymbol(1, [0,0])
     printGrid()
+    
     gameOver = True
