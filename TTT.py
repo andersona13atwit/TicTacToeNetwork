7174a569e3e6029d1dda5a6854104d680ae9ba5d
andersona13 <andersona13@wit.edu>
2022-10-28 14:37:55 -0400

    Commiting fixes and error handling

--- TTT.py
+++ TTT.py
@@ -121,35 +121,18 @@ printGrid()
 message = f'Player {currentPlayer}, please enter a position'
 print('hint: formatting for adding an o to the top left corner should look like : o 1,1')
 turnCounter = 0
-# test comment for commiting
 while(not(gameOver)):
     message = f'Player {(currentPlayer)}, please enter a position: '
-    try:
-        stringInput = input(message)
-        location = stringInput.split()
-        symToAdd = 0 if location[0] == 'o' else 1
-        location = location[1].split(',')
-        for num in range(len(location)):
-            location[num] = int(location[num])
-            location[num]-=1
-        locationnumber=[int(i) for i in location]      
-        if grid[locationnumber[0]][locationnumber[1]] ==2  :
-            currentPlayer = (currentPlayer%2)+1
-            addSymbol(symToAdd,location)
-            printGrid()
-            print()
-        else:
-            print('Dont replace other players work')
-            turnCounter -= 1 
-    except :
-        print('Please type in correct format')
-        turnCounter -= 1 
-
+    inputs = formatInput(input(message))
+    addSymbol(inputs[0],inputs[1])
+    currentPlayer = (currentPlayer%2)+1
+    printGrid()
+    print()
     end = checkWin()
-    
     if(end < 0 or turnCounter == 9):
         print('game over!')
         gameOver = True
+     
     else:
         turnCounter+=1
-        print("Turn:",turnCounter)
+        print(turnCounter)
