a5be8c368e8319eff6ba9b666ddbb9268def10f2
andersona13 <andersona13@wit.edu>
2022-10-28 15:33:02 -0400

    Fixed a bug in stringToGrid

--- Client.py
+++ Client.py
@@ -10,19 +10,20 @@
 # print(s.recv(50))
 # s.close()
 
-initGrid = [1, 2, 3, 
-        4, 5, 6,
-        7, 8, 9]
+initGrid = [[1, 2, 3], 
+        [4, 5, 6],
+        [7, 8, 9]]
 def stringToGrid(incomingString):
-    gridStr = (str(incomingString).strip('[').strip(']').replace(' ',''))
-    arr1 = gridStr[0:6].split(',')
-    arr2 = gridStr[6:12].split(',')
+    arr1 = incomingString[0:6].split(',')
+    arr2 = incomingString[6:12].split(',')
     arr2.pop(len(arr2)-1)
-    arr3 = gridStr[12:17].split(',')
+    arr3 = incomingString[12:17].split(',')
     arr1.pop(len(arr1)-1)
     grid = [arr1, arr2, arr3]
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             grid[x][y] = int(grid[x][y])
     return grid
-print(stringToGrid(initGrid))
+
+print(bytes(str(initGrid),'utf-8'))
+# print(stringToGrid(initGrid))
