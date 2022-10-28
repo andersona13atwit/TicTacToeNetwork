6b68626248240200bbc50df7cab950a47735d29b
andersona13 <andersona13@wit.edu>
2022-10-28 15:19:42 -0400

    Added method to convert a string to a 2d 9x9 array

--- Client.py
+++ Client.py
@@ -1,11 +1,25 @@
-import socket
+# import socket
 
-host = ''
+# host = ''
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, 1234))
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((host, 1234))
 
-s.send(b'Hello!')
+# s.send(b'Hello!')
     
-print(s.recv(50))
-s.close()
+# print(s.recv(50))
+# s.close()
+
+initGrid = [1, 2, 3, 
+        4, 5, 6,
+        7, 8, 9]
+def stringToGrid(incomingString):
+    gridStr = (str(incomingString).strip('[').strip(']').replace(' ',''))
+    arr1 = gridStr[0:6].split(',')
+    arr2 = gridStr[6:12].split(',')
+    arr2.pop(len(arr2)-1)
+    arr3 = gridStr[12:17].split(',')
+    arr1.pop(len(arr1)-1)
+    grid = [arr1, arr2, arr3]
+    return grid
+print(stringToGrid(initGrid))
