0e4a8a80985807b4b8048e8178a19765cbfe3ddb
andersona13 <andersona13@wit.edu>
2022-11-04 17:18:26 -0400

    Added documentation

--- Client.py
+++ Client.py
@@ -1,24 +1,32 @@
-# import socket
-
-# host = ''
-
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((host, 1234))
-
-# s.send(b'Hello!')
-    
-# print(s.recv(50))
-# s.close()
-
-initGrid = [[1, 2, 3], 
-        [4, 5, 6],
-        [7, 8, 9]]
+import socket
+grid = [[2, 2, 2], 
+        [2, 2, 2], 
+        [2, 2, 2]]
 def stringifyInput(incString):
+    """A method to turn a byte-string version of an array into a normal string version of an array without the parentheses
+        Made to be used within stringToGrid
+
+    Args:
+        incString (byte-string): A series of bytes ideally originating from the server
+                                 Should be a string/byte version of an array
+
+    Returns:
+        incString: Returns the original byte-string as a normal string to be parsed by stringToGrid()
+    """
     incString = incString.decode()
     incString = incString.replace('[','')
     incString = incString.replace(']', '')
     return incString
+
 def stringToGrid(incomingString):
+    """A method to turn a formatted string into a 2d array. The string must have 9 comma-spaced elements
+
+    Args:
+        incomingString (byte-string): The string to be turned into an array
+
+    Returns:
+        grid: A completed 2d array from the inputted values
+    """
     incomingString = stringifyInput(incomingString)
     tempHolder = []
     grid = []
@@ -36,8 +44,67 @@ def stringToGrid(incomingString):
             count = 0
             toAppend = []
     return grid
-initString = bytes(str(initGrid),'utf-8')
-print(stringToGrid(initString))
+def printGrid():
+    """
+    Formats the grid for output (should be removed before final implementation)
+    """
+    for row in grid:
+        for col in row:
+            if(col == 1):
+                print('x', end=' ')
+            elif(col == 0):
+                print('o', end=' ')
+            else:
+                print('-', end=' ')
+        print('')
+def formatInput(stringInput):
+    """Takes an input string and splits it into an int that represents a symbol and a location array of 2 ints that is 0<value<4
+
+    Args:
+        stringInput (String): A string in the format of "symbol int,int". Any deviance from this will be handled between here and addSymbol
+
+    Returns:
+        array: returns the two inputs that are needed for addSymbol, a symbol and a location
+    """
+    location = stringInput.split()
+    if(len(location) != 2):
+        print('You did not enter enough characters, please try another input')
+        return formatInput(input('Please enter a position: '))
+    symToAdd = 0 if location[0] == 'o' else 1 if location[0] == 'x' else 2
+    if(len(location[1]) != 3):
+        pass
+    location = location[1].split(',')
+    for num in range(len(location)):
+        location[num] = int(location[num])
+        location[num] -= 1
+    return [symToAdd, location]
+def addSymbol(symbol, location):
+    """
+    Adds symbol to grid at location\n
+    Args:
+        symbol (int): an integer that (if less than 2) will be added to the grid
+        location (int[]): acts as coordinates in the grid and specifies where to put the symbol
+    """
+    if symbol < 2:
+        if grid[location[0]][location[1]] == 2:
+            grid[location[0]][location[1]] = symbol
+        else:
+            print('you entered an occupied space! Please try again')
+            inputs = formatInput(input('Please enter a position: '))
+            addSymbol(inputs[0], inputs[1])
+    else:
+        print('you entered an illegal symbol! Please try again (remember, only x and o are accepted')
+        inputs = formatInput(input('Please enter a position: '))
+        addSymbol(inputs[0], inputs[1])
+            
 
 
-# print(stringToGrid(initGrid))
+host = '10.220.43.220'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, 1234))
+
+s.send(b'Hello!')
+
+print(stringToGrid(s.recv(50)))
+s.close()
--- Server.py
+++ Server.py
@@ -1,15 +1,159 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 1234))
-s.listen(5)
+currentPlayer = 1
+grid = [[2, 2, 2], 
+        [2, 2, 2], 
+        [2, 2, 2]]
 
+def checkWin():
+    """Checks for three win conditions, horizontal, vertical, and diagonal 
+
+    Returns:
+        int: returns -1 if a win has been detected ANYWHERE, and a 0 if otherwise
+    """
+    # Checks for vertical win
+    for vert in grid:
+        if(vert == [0, 0, 0]):
+            print('O wins!')
+            return -1
+        if(vert == [1, 1, 1]):
+            print('X wins!')
+            return -1
+    # Checks for horizontal win
+    for col in range(3):
+        if grid[0][col] == 0 and grid[1][col] == 0 and grid[2][col] == 0 :
+            print('O wins!')
+            return -1
+    for col in range(3):
+        if grid[0][col] == 1 and grid[1][col] == 1 and grid[2][col] == 1 :
+            print('X wins!')
+            return -1   
+
+    #Checks for diagonal win
+    if grid[0][0] == 0 and grid[1][1] == 0 and grid[2][2] == 0 :
+        print('O wins!')
+        return -1
+    if grid[0][0] == 1 and grid[1][1] == 1 and grid[2][2] == 1 :
+        print('X wins!')
+        return -1
+
+    if grid[2][0] == 0 and grid[1][1] == 0 and grid[0][2] == 0 :
+        print('O wins!')
+        return -1
+    if grid[2][0] == 1 and grid[1][1] == 1 and grid[0][2] == 1 :
+        print('X wins!')
+        return -1
+    
+    return 0
+    
+    
+def printGrid():
+    """
+    Formats the grid for output (should be removed before final implementation)
+    """
+    for row in grid:
+        for col in row:
+            if(col == 1):
+                print('x', end=' ')
+            elif(col == 0):
+                print('o', end=' ')
+            else:
+                print('-', end=' ')
+        print('')
+def formatInput(stringInput):
+    """Takes an input string and splits it into an int that represents a symbol and a location array of 2 ints that is 0<value<4
+
+    Args:
+        stringInput (String): A string in the format of "symbol int,int". Any deviance from this will be handled between here and addSymbol
+
+    Returns:
+        array: returns the two inputs that are needed for addSymbol, a symbol and a location
+    """
+    location = stringInput.split()
+    if(len(location) != 2):
+        print('You did not enter enough characters, please try another input')
+        return formatInput(input(f'Player {(currentPlayer)}, please enter a position: '))
+    symToAdd = 0 if location[0] == 'o' else 1 if location[0] == 'x' else 2
+    if(len(location[1]) != 3):
+        pass
+    location = location[1].split(',')
+    for num in range(len(location)):
+        location[num] = int(location[num])
+        location[num] -= 1
+    return [symToAdd, location]
+def addSymbol(symbol, location):
+    """
+    Adds symbol to grid at location\n
+    Args:
+        symbol (int): an integer that (if less than 2) will be added to the grid
+        location (int[]): acts as coordinates in the grid and specifies where to put the symbol
+    """
+    if symbol < 2:
+        if grid[location[0]][location[1]] == 2:
+            grid[location[0]][location[1]] = symbol
+        else:
+            print('you entered an occupied space! Please try again')
+            inputs = formatInput(input(f'Player {currentPlayer}, please enter a position: '))
+            addSymbol(inputs[0], inputs[1], currentPlayer)
+    else:
+        print('you entered an illegal symbol! Please try again (remember, only x and o are accepted')
+        inputs = formatInput(input(f'Player {currentPlayer}, please enter a position: '))
+        addSymbol(inputs[0], inputs[1])
+            
+            
+    
+
+gameOver = False
+printGrid()
+message = f'Player {currentPlayer}, please enter a position'
+print('hint: formatting for adding an o to the top left corner should look like : o 1,1')
+turnCounter = 0
+while(not(gameOver)):
+    message = f'Player {(currentPlayer)}, please enter a position: '
+    inputs = formatInput(input(message))
+    addSymbol(inputs[0],inputs[1])
+    currentPlayer = (currentPlayer%2)+1
+    printGrid()
+    print()
+    end = checkWin()
+    if(end < 0 or turnCounter == 8):
+        print('game over!')
+        gameOver = True
+     
+    else:
+        turnCounter+=1
+        print(turnCounter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('10.220.43.220', 1234))
+s.listen(5)
+initGrid = [[1, 2, 3], 
+        [4, 5, 6],
+        [7, 8, 9]]
 while True:
     clientsocket, address = s.accept()
     print(address)
     
     message = clientsocket.recv(50) + (b'Hello Client!')
-    clientsocket.send(bytes(message))
+    clientsocket.send(bytes(str(initGrid),'utf-8'))
     print(message)
     
     clientsocket.close()
